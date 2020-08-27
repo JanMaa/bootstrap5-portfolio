c988f921692db9730fa0f45ad7100c54b9630a88
janric <janricmalate@gmail.com>
2020-08-27 22:57:56 +0800

    modified settings

--- bs5portfolio/settings.py
+++ bs5portfolio/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = 'hcr-a6bjzm01vaxdexprh(tsy9v48ro4&x34k0-@5)_27)t-^d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['janric-malate.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
